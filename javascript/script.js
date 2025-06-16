a9c864c923da5e65f1787bc5fdbab37b4d4d3d47
geerthana-karuppiah <geerthana.26j24@kprcas.ac.in>
2025-06-16 15:38:59 +0530

    completed

---
+++ javascript/script.js
@@ -0,0 +1,57 @@
+var a = "10";
+var b = 10;
+
+console.log(a==b);//true
+console.log(a===b//false
+console.log(a!=b);//false
+console.log(a!==b);//true
+
+var num=2;
+var result=num%2==0?"Even":"odd";
+console.log(result);
+
+var mark=34;
+var result=(mark>=80)?"0":(mark>=60?"A+">35?"pass":"fail");
+console.log(result);
+function sum (a,b)
+console.log(a+b)
+
+sum(10,20)
+
+var sum =()=>{
+    var a=10;
+    var b=20;
+    console.log(a=b)
+}
+sum();
+
+let arr=[10,20,30,40];
+let arr2=[...arr,50,60,70]
+console.log(arr)
+console.log(arr2)
+
+
+var marks=[90,95,96,100,99];
+var [m1,m2,m3,m4,m5]=marks;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5);
+
+}
+
+if(mark>=80 && mark<<=100)}
+    console.log("0");
+}
+
+
+
+console.log(type a);
+console.log(typeof b);
+a = true;
+console.log(typeof a)
+var c;
+console.log(typeof c);
+var c = null;
+console.log(c);
